c8b742de2bc9e63bc7e01ca3c989c49eebe5b5d7
Nate River <hippogamesunity@gmail.com>
2017-11-20 18:47:04 +0300

    upd

---
+++ csharp-cgdk/MyClasses/Unit.cs
@@ -0,0 +1,34 @@
+﻿using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyClasses
+{
+    /// <summary>
+    /// Vehicle wrapper
+    /// </summary>
+    public class VehicleWrapper
+    {
+        public long Id;
+        public VehicleType Type;
+        public double X;
+        public double Y;
+        public int Durability;
+        public long PlayerId;
+
+        public VehicleWrapper(Vehicle vehicle)
+        {
+            Id = vehicle.Id;
+            Type = vehicle.Type;
+            X = vehicle.X;
+            Y = vehicle.Y;
+            Durability = vehicle.Durability;
+            PlayerId = vehicle.PlayerId;
+        }
+
+        public void Update(VehicleUpdate vehicleUpdate)
+        {
+            X = vehicleUpdate.X;
+            Y = vehicleUpdate.Y;
+            Durability = vehicleUpdate.Durability;
+        }
+    }
+}
--- csharp-cgdk/MyStrategy.cs
+++ csharp-cgdk/MyStrategy.cs
@@ -1,19 +1,85 @@
+using System.Collections.Generic;
+using System.Linq;
 using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
+using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyClasses;
 
 namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk {
-    public sealed class MyStrategy : IStrategy {
-        public void Move(Player me, World world, Game game, Move move) {
-            if (world.TickIndex == 0) {
+    public sealed class MyStrategy : IStrategy
+    {
+        private readonly Dictionary<long, VehicleWrapper> _units = new Dictionary<long, VehicleWrapper>();
+
+        public void Move(Player me, World world, Game game, Move move)
+        {
+            UpdateUnits(world);
+
+            if (world.TickIndex == 0) // Stupid tick system. 1 tick = 1 action
+            {
                 move.Action = ActionType.ClearAndSelect;
                 move.Right = world.Width;
                 move.Bottom = world.Height;
                 return;
             }
 
-            if (world.TickIndex == 1) {
+            if (world.TickIndex == 1)
+            {
                 move.Action = ActionType.Move;
-                move.X = world.Width / 2.0D;
-                move.Y = world.Height / 2.0D;
+                move.X = world.Width / 2;
+                move.Y = world.Height / 2;
+            }
+
+            if (world.TickIndex == 3)
+            {
+                SelectFighters(world, move);
+            }
+
+            if (world.TickIndex >= 4)
+            {
+                MoveFigters(me, move);
+            }
+        }
+
+        private void UpdateUnits(World world)
+        {
+            foreach (var newVehicle in world.NewVehicles)
+            {
+                _units.Add(newVehicle.Id, new VehicleWrapper(newVehicle));
+            }
+
+            foreach (var vehicleUpdate in world.VehicleUpdates)
+            {
+                if (vehicleUpdate.Durability > 0)
+                {
+                    _units[vehicleUpdate.Id].Update(vehicleUpdate);
+                }
+                else
+                {
+                    _units.Remove(vehicleUpdate.Id);
+                }
+            }
+        }
+
+        private static void SelectFighters(World world, Move move)
+        {
+            move.Action = ActionType.ClearAndSelect;
+            move.Right = world.Width;
+            move.Bottom = world.Height;
+            move.VehicleType = VehicleType.Fighter;
+        }
+
+        private void MoveFigters(Player me, Move move)
+        {
+            var target = _units.Values.FirstOrDefault(i => i.Type == VehicleType.Helicopter && i.PlayerId != me.Id);
+
+            if (target != null) // Fighters can't attack only helicopters
+            {
+                var myFighter = _units.Values.FirstOrDefault(i => i.Type == VehicleType.Fighter && i.PlayerId == me.Id);
+
+                if (myFighter != null)
+                {
+                    move.Action = ActionType.Move;
+                    move.X = target.X - myFighter.X;
+                    move.Y = target.Y - myFighter.Y;
+                }
             }
         }
     }
