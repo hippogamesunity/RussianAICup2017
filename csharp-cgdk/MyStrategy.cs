ad7b2c509404d43898244548ad2264bdac1b5844
Nate River <hippogamesunity@gmail.com>
2017-11-21 11:32:11 +0300

    upd

--- csharp-cgdk/MyStrategy.cs
+++ csharp-cgdk/MyStrategy.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
-using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.MyClasses;
 
 namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk {
     public sealed class MyStrategy : IStrategy
@@ -126,6 +125,36 @@ namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk {
                     return;
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// Vehicle wrapper
+    /// </summary>
+    public class VehicleWrapper
+    {
+        public long Id;
+        public VehicleType Type;
+        public double X;
+        public double Y;
+        public int Durability;
+        public long PlayerId;
+
+        public VehicleWrapper(Vehicle vehicle)
+        {
+            Id = vehicle.Id;
+            Type = vehicle.Type;
+            X = vehicle.X;
+            Y = vehicle.Y;
+            Durability = vehicle.Durability;
+            PlayerId = vehicle.PlayerId;
+        }
+
+        public void Update(VehicleUpdate vehicleUpdate)
+        {
+            X = vehicleUpdate.X;
+            Y = vehicleUpdate.Y;
+            Durability = vehicleUpdate.Durability;
         }
     }
 }
